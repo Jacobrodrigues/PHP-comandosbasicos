d9f521b4be419840a003a892efa344f37505eb56
Jacob Rodrigues <jacob.neto@hotmail.com>
2019-05-02 16:22:53 -0300

    Add files via upload

--- Laços de repetição.php
+++ Laços de repetição.php
@@ -14,3 +14,13 @@ do{
 	echo $i;
 	$i++;
 } while ($i < 10);
+#for 
+echo '<br> for: ';
+for ($i=0; $i < 10; $i++) { 
+	echo $i;	
+}
+#forach
+$i = [0,1,2,3,4,5,6,7,8,9];
+foreach ($i as $j) {
+	echo $j;
+}
